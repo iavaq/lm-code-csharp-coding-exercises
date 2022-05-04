c7b8b248678d7585d5f2b42513e12c8c1d572583
Ia <iavaquilar@gmail.com>
2022-05-04 22:30:14 +0100

    Exercise001 completed

--- Exercises/Exercise001.cs
+++ Exercises/Exercise001.cs
@@ -9,31 +9,42 @@ namespace Exercises
         public string CapitalizeWord(string word)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return word[0].ToString().ToUpper() + word.Substring(1);
         }
 
         public string GenerateInitials(string firstName, string lastName)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return firstName[0].ToString().ToUpper() + "." + lastName[0].ToString().ToUpper();
         }
 
         public double AddVat(double originalPrice, double vatRate)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return Math.Round(originalPrice + (originalPrice*(vatRate/100)), 2);
         }
 
         public string Reverse(string sentence)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            char[] sentenceArray = sentence.ToCharArray();
+            return Array.Reverse(sentenceArray);
         }
 
         public int CountLinuxUsers(List<User> users)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            int linuxUsers = 0;
+            foreach (User u in users) {
+                if (u.Type == "Linux")
+                    linuxUsers++;
+            }
+            return linuxUsers;
         }
     }
 }
